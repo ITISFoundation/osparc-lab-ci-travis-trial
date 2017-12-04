01291af91c7adab55ca10a199f3fd6f0f6c28d2c
Stefan Benkler <benkler@zurichmedtech.com>
2017-12-04 14:36:06 +0100

    Fixing the tests again. Be more verbose.

--- Package1/test1.py
+++ Package1/test1.py
@@ -1,8 +1,10 @@
 import sys
 print "Hello world from Test 1"
 
-exit_code = 1
+exit_code = 0
 if exit_code != 0:
 	print "This shall fail now with exit code ", exit_code
+else:
+	print "I think, this was a success :)"
 
 sys.exit(exit_code)
