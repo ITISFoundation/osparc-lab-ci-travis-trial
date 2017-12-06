7ed8feccdfc89e79e0716c1a8a2f4eb54a76c41c
Stefan Benkler <benkler@itis.ethz.ch>
2017-12-06 09:37:27 -0500

    Overall PEP8: added script to check it and adapted the remaining Python scripts

--- Package2/test2.py
+++ Package2/test2.py
@@ -1,10 +1,17 @@
+''' Testing Package2 '''
+
 import sys
-print "Hello world from Test 2"
 
-exit_code = 0
-if exit_code != 0:
-	print "This shall fail now with exit code ", exit_code
-else:
-	print "I think, this was a success :)"
+def main():
+    '''Main test function'''
+    print "Hello world from Test 2"
 
-sys.exit(exit_code)
+    exit_code = 0
+    if exit_code != 0:
+        print "Test 2 shall fail now with exit code ", exit_code
+    else:
+        print "I think, test 2 was a success :)"
+    return exit_code
+
+if __name__ == '__main__':
+    sys.exit(main())
--- Package4/test4.py
+++ Package4/test4.py
@@ -1,10 +1,17 @@
+''' Testing Package4 '''
+
 import sys
-print "Hello world from Test 4"
 
-exit_code = 0
-if exit_code != 0:
-	print "This shall fail now with exit code ", exit_code
-else:
-	print "I think, this was a success :)"
+def main():
+    '''Main test function'''
+    print "Hello world from Test 4"
 
-sys.exit(exit_code)
+    exit_code = 0
+    if exit_code != 0:
+        print "Test 4 shall fail now with exit code ", exit_code
+    else:
+        print "I think, test 4 was a success :)"
+    return exit_code
+
+if __name__ == '__main__':
+    sys.exit(main())
