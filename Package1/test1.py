db3adedc375ba93f304f9f0af429b9be45be8f1f
Stefan Benkler <benkler@zurichmedtech.com>
2017-12-06 11:28:08 +0100

    Tried to make the script pep8 compatible. Removed report from test output

--- Package1/test1.py
+++ Package1/test1.py
@@ -1,10 +1,12 @@
+''' Testing Package1 '''
+
 import sys
 print "Hello world from Test 1"
 
-exit_code = 0
-if exit_code != 0:
-	print "This shall fail now with exit code ", exit_code
+exitCode = 0
+if exitCode != 0:
+    print "This shall fail now with exit code ", exitCode
 else:
-	print "I think, this was a success :)"
+    print "I think, this was a success :)"
 
 sys.exit(exit_code)
